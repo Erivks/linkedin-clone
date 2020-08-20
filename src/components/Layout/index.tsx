a19fde205cecc7e8d01111878bc03c8d8839df3f
Erivks <mordecai.es27@gmail.com>
2020-08-20 16:40:33 -0300

    Declarando Header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import LeftColumn from '../LeftColumn'
 import MiddleColumn from '../MiddleColumn';
 import RightColumn from '../RightColumn';
-
+import MobileHeader from '../MobileHeader';
 import { Container } from './styles';
 
 const Layout: React.FC = () => {
   return (
     <Container>
+        <MobileHeader />
         <main>
             <LeftColumn />
             <MiddleColumn />
