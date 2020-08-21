f356793c53d96569598d888931ea59467420d911
Erivks <mordecai.es27@gmail.com>
2020-08-21 11:57:13 -0300

    Gerenciamento de Loading para Shimmer Effect

--- src/components/LeftColumn/ProfilePanel/index.tsx
+++ src/components/LeftColumn/ProfilePanel/index.tsx
@@ -10,12 +10,12 @@ const ProfilePanel: React.FC = () => {
       <Container>
         <div className="profile-cover"></div>
         <img
-          src="https://github.com/guilhermerodz.png"
+          src="https://github.com/Erivks.png"
           alt="Avatar"
           className="profile-picture"
         />
-        <h1>Guilherme Rodz</h1>
-        <h2>Software Engineer @ Rocketseat</h2>
+        <h1>Erick Oliveira</h1>
+        <h2>Desenvolvedor Web @ Bitix</h2>
 
         <div className="separator"></div>
 
